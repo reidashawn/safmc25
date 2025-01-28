b469076a152c3e7605a44a11ab94d81b4c1ed9af
shawn <shawnkengkiat@gmail.com>
2025-01-28 18:15:55 +0000

    feat: integrates ros2 subscription with pyqt

--- src/gui/gui/pyqt.py
+++ src/gui/gui/pyqt.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
 from PyQt5.QtGui import QIcon, QFont, QPixmap, QPalette, QColor, QPainter, QImage
 from PyQt5.QtCore import Qt, QRectF, QRect, QThread, pyqtSignal
 
+
 margin = 10
 camera_width, camera_height = 900, 600
 label_height = 30
