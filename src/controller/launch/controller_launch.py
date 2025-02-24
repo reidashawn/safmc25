2f9d82191b28a82090d15e1a455d64d75c5da5dd
Rei <sleepyyreii.gmail.com>
2025-02-25 01:16:43 +0800

    launch file

--- src/controller/launch/controller_launch.py
+++ src/controller/launch/controller_launch.py
@@ -16,18 +16,26 @@ def generate_launch_description():
         description='Baud rate for serial communication'
     )
 
-    return launch.LaunchDescription([
+    hand_arg = DeclareLaunchArgument(
+        'hand',
+        default_value='right',
+        description='which hand the controller is on'
+    )
+
+    return launch.LaunchDescription(
         serial_port_arg,
         baud_rate_arg,
+        hand_arg,
         
         # Controller Publisher Node
         launch_ros.actions.Node(
             package='controller',
             executable='controller_pub',
-            name='controller_pub',
+            name='controller_pub'+ LaunchConfiguration('hand'),
             parameters=[{
                 'serial_port': LaunchConfiguration('serial_port'),
                 'baud_rate': LaunchConfiguration('baud_rate'),
+                'hand': LaunchConfiguration('hand')
             }],
             output='screen'
         ),
@@ -36,7 +44,21 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='controller',
             executable='imu_converter',
-            name='imu_converter',
+            name='imu_converter_' + LaunchConfiguration('hand'),
+            parameters=[{
+                'hand': LaunchConfiguration('hand')
+            }],
+            output='screen'
+        ),
+
+        # Potentiometer Converter Node
+        launch_ros.actions.Node(
+            package='controller',
+            executable='pot_converter',
+            name= 'pot_converter_' + LaunchConfiguration('hand'),
+            parameters=[{
+                'hand': LaunchConfiguration('hand')
+            }],
             output='screen'
         )
-    ])
+    )
