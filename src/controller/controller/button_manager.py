3b83ec352d7b770b083153a417a95335f52b7cde
shawn chan <shawnkengkiat@gmail.com>
2025-02-24 14:27:54 +0000

    feat: adds waiting for future

--- src/controller/controller/button_manager.py
+++ src/controller/controller/button_manager.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
 from mavros_msgs.srv import CommandBool, SetMode, CommandLong
+from mavros_msgs.msg import State
 from helpers.button import Button
 from time import sleep
 
@@ -21,14 +22,20 @@ class ButtonManagerNode(Node):
     def __init__(self):
         super().__init__('button_manager')
 
+        self.is_armed = False
 
         self.button1 = Button(topic='controller/but1', short_callback=self.arm_takeoff_callback)
-        self.button2 = Button(topic='controller/but2', short_callback=self.takeoff)
+        # self.button2 = Button(topic='controller/but2', short_callback=self.takeoff)
+        self.button3 = Button(topic='controller/but3', short_callback=self.land_callback)
+        self.button4 = Button(topic='controller/but4', short_callback=self.set_guided_callback)
 
+        # RIGHT CONTROLLER BUTTONS
         self.but1_subscriber = self.create_subscription(Int32, 'controller/but1', self.button1.data_callback, 10)
-        self.but2_subscriber = self.create_subscription(Int32, 'controller/but2', self.button2.data_callback, 10)
-        # self.but3_subscriber = self.create_subscription(Int32, 'controller/but3', 10)
-        # self.but4_subscriber = self.create_subscription(Int32, 'controller/but4', 10)
+        # self.but2_subscriber = self.create_subscription(Int32, 'controller/but2', self.button2.data_callback, 10)
+        self.but3_subscriber = self.create_subscription(Int32, 'controller/but3', self.button3.data_callback, 10)
+        self.but4_subscriber = self.create_subscription(Int32, 'controller/but4', self.button4.data_callback, 10)
+        # MAVROS STATE SUBSCRIPTION
+        self.is_armed_subscriber = self.create_subscription(State, '/mavros/state', self.is_armed_callback, 10)
 
     
         self.mavros_clients = {
@@ -47,10 +54,17 @@ class ButtonManagerNode(Node):
         self.get_logger().info("arm and takeoff")
         self.arm_drone()
             
-
+    def land_callback(self):
+        self._change_mode('LAND')
     
+    def set_guided_callback(self):
+        self._change_mode('GUIDED')
 
-    def change_mode(self, mode: str):
+    def is_armed_callback(self, msg: State):
+        self.is_armed = msg.armed
+
+
+    def _change_mode(self, mode: str):
         mode_upper = mode.upper()
         if mode_upper not in VALID_MODES:
             self.get_logger().error(f"{mode.upper()} is not a valid mode")
@@ -62,8 +76,9 @@ class ButtonManagerNode(Node):
     def arm_drone(self):
         arm_req = CommandBool.Request(value=True)
         future = self.mavros_clients['arm'].call_async(arm_req)
-        self.get_logger().info(f"Arm drone result: {future.result()}")
-        return future.done()
+        rclpy.spin_until_future_complete(self, future)
+        print(future.result().success)
+        return future.result()
 
     def takeoff(self):
         takeoff_req = CommandLong.Request(
@@ -81,8 +96,7 @@ class ButtonManagerNode(Node):
         future = self.mavros_clients['takeoff'].call_async(takeoff_req)
         self.get_logger().info(f"Takeoff command result: {future.result()}")
 
-    def land_callback(self):
-        self.change_mode('LAND')
+    
 
 def main(args=None):
     rclpy.init(args=args)
