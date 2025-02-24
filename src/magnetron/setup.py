93b3595aa32029ed206951940755557ef5c3a7b6
shawn chan <shawnkengkiat@gmail.com>
2025-02-24 15:32:52 +0000

    ugh

--- src/magnetron/setup.py
+++ src/magnetron/setup.py
@@ -1,16 +1,18 @@
+import os
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'magnetron'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-        data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+    packages=find_packages( where='.'),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Ensure glob() returns a valid list
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/*.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -18,7 +20,8 @@ setup(
     maintainer_email='shawnkengkiat@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    # install_requires=['setuptools', 'pytest'],
+
     entry_points={
         'console_scripts': [
             'toggle_io = magnetron.toggle_io:main',
