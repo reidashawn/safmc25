cdf33064ccf28bf2df456232779f1af270656442
Rei <sleepyyreii.gmail.com>
2025-03-17 22:56:26 +0800

    changed launch file

--- src/magnetron/launch/launch.py
+++ src/magnetron/launch/launch.py
@@ -1,27 +1,21 @@
 import launch
 import launch_ros.actions
-import launch
-import launch_ros.actions
-import os
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Get MAVROS package share directory
-    mavros_launch_file = os.path.join(
-        get_package_share_directory('mavros'), 'launch', 'apm.launch'
-    )
-
     return launch.LaunchDescription([
-        # Include MAVROS APM launch file without any additional parameters
-        launch.actions.IncludeLaunchDescription(
-            launch.launch_description_sources.PythonLaunchDescriptionSource(mavros_launch_file),
-        ),
-
-        # Custom Nodes from Magnetron
+        # Launch stepper_node
         launch_ros.actions.Node(
             package='magnetron',
             executable='stepper_node',
             name='stepper_node',
+            output='screen'
+        ),
+
+        # Launch servo_node
+        launch_ros.actions.Node(
+            package='magnetron',
+            executable='servo_node',
+            name='servo_node',
             output='screen'
         )
     ])
